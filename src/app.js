be7a1cac1b2ca04c687c5b2a72fe92221494ec46
Tom Martin <tom@tpgmartin.com>
2016-07-17 18:13:18 +0100

    Add math utils

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 "use strict";
 
-global.Constructor = require("./Constructor");
-global.JSGameEngine = require("./JSGameEngine");
-global.Component = require("./Component");
-global.Time = require("./Time");
-global.Vector2 = require("./Vector2");
-global.Transform = require("./Transform");
-global.GameObject = require("./GameObject");
-global.Sprite = require("./Sprite");
+// Number corresponds to order required
+/* 1. */ global.Constructor = require("./Constructor");
+/* 2. */ global.JSGameEngine = require("./JSGameEngine");
+/* 3. */ global.Math = require("./Math");
+/* 4. */ global.Component = require("./Component");
+/* 5. */ global.Time = require("./Time");
+// /* 6. */ global.Physics2D = require("./Physics2D");
+/* 7. */ global.Vector2 = require("./Vector2");
+/* 8. */ global.Transform = require("./Transform");
+// /* 9. */ global.Shadow = require("./Shadow");
+// /* 10. */ global.Input = require("./Input");
+/* 11. */ global.GameObject = require("./GameObject");
+// /* 12. */ global.Color = require("./Color");
+// /* 13. */ global.Text = require("./Text");
+/* 14. */ global.Sprite = require("./Sprite");
+// /* 15. */ global.Particle = require("./Particle");
+// /* 16. */ global.ParticleSystem = require("./ParticleSystem");
+// /* 17. */ global.AudioClip = require("./AudioClip");
+// /* 18. */ global.Background = require("./Background");
